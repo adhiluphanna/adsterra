27c5276a400fa2baf5d82a7a6dd8519ee9d1cf12
adhiluphanna <39770390+adhiluphanna@users.noreply.github.com>
2021-10-01 23:50:59 +0700

    Update and rename zupedia.js to directlink.js

---
+++ directlink.js
@@ -0,0 +1,29 @@
+var pages = [ 
+
+"https://www.effectivecpmgate.com/akh7cfy0s?key=85eedacc6f9080667d09e90ec46bbe78",
+"https://www.effectivecpmgate.com/e33xmypmu?key=9ccad1e11880c35ce8f03453c7868d04",
+"https://www.effectivecpmgate.com/fv8hvkea?key=b2a79e0bab2031163d784183e018aa32",
+"https://www.effectivecpmgate.com/fy5jvdb6er?key=a2d372865650fa92d16ff8a9aca84c08",
+"https://www.effectivecpmgate.com/gm3sa354w?key=fd9666fe0dd8b18f02eae1925c6b3b48",
+"https://www.effectivecpmgate.com/ian5sddbhn?key=cab0e894642dc56f27b8ada7e2ed25ff",
+"https://www.effectivecpmgate.com/ib553a7f?key=eb5ff9a09aede3c9193c188e468a6530",
+"https://www.effectivecpmgate.com/je6mvi5z?key=4f62984fcd8eedeaa3658115e87b1504",
+"https://www.effectivecpmgate.com/jsqixcjg0?key=b41e77ab60f778820d10d6713f59b408",
+"https://www.effectivecpmgate.com/m3mt6q24?key=d95d6095e4fac18df6a7a817871e1c41",
+"https://www.effectivecpmgate.com/mfetaga5jq?key=c03cfedcf6388f911dce716f063a8d5e",
+"https://www.effectivecpmgate.com/mftq4y8w8?key=6961aac94ba5530f814973783bec98e0",
+"https://www.effectivecpmgate.com/p62r7t1g?key=3dea117232d8de1e4986fd4b93c1d19e",
+"https://www.effectivecpmgate.com/rtguqt6gk?key=b5db3a320ba507a92a0512608ad8d10a",
+"https://www.effectivecpmgate.com/u5sr9965xe?key=4ef3d7d56a9ae04ad42c891cdff76b7a",
+"https://www.effectivecpmgate.com/vetxzi7f?key=b4f78df472659153a5792364ea8e96a8",
+"https://www.effectivecpmgate.com/vfenh94ucy?key=a121d703c5ac1be24e9b541bc305a0eb",
+"https://www.effectivecpmgate.com/winir4sa?key=0fe7b34f3d199c9ed6a275b17f8ba005",
+"https://www.effectivecpmgate.com/x75y7kj4?key=39e47f1f02a5fa14e64c6739fa36c698",
+"https://www.effectivecpmgate.com/ys5zk6m7n?key=26b3c19b88bebcaea8e9706448d758fe",
+"https://www.effectivecpmgate.com/znn0w3rs?key=59710ef8679cc3ad2e7c10ef26b9eff0",
+
+]; 
+function randomPage() { 
+return pages[Math.round(Math.random() * (pages.length - 1))]; 
+} 
+location.href= randomPage(); 
